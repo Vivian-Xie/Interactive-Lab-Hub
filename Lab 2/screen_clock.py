6aa88f5c30079bd219a8718dcdad20d10bf1d137
xz946-maker <xz946@cornell.edu>
2025-09-15 13:03:36 -0400

    Update screen_clock.py

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -65,6 +65,12 @@ while True:
     draw.rectangle((0, 0, width, height), outline=0, fill=400)
 
     #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
+    current_time = time.strftime("%H:%M:%S")
+    current_date = time.strftime("%Y-%m-%d")
+    y = 20
+    draw.text((x, y), current_time, font=font, fill=(255, 255, 255))
+    draw.text((8, height - 26), current_date, font=font, fill=(180, 180, 180))
+
 
     # Display image.
     disp.image(image, rotation)
